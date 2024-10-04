68778bf1d048edac0e66fd266aa6eb03e2db8148
hshsdrop <i1.kuznetsv@gmail.com>
2024-10-04 20:48:28 +0300

    Delete src directory

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-from .add_features import *
-from .clean_data import *
-from .get_data import *
-from .make_prediction import *
-from .config import *
--- src/get_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def get_data(path: str) -> pd.DataFrame:
-    """Function loads Data from specific path.
-    :param path: data path.
-    :return: DataFrame.
-    """
-    data = pd.read_csv(path)
-    return data
