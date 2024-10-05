d75df33a3461ed9c0e075b3e3a49ba2f10d302c2
hshsdrop <i1.kuznetsv@gmail.com>
2024-10-05 14:59:13 +0300

    testing and rewrite kmeans model

--- testing_kmeans.py
+++ testing_kmeans.py
@@ -1,39 +1,10 @@
+# from loguru import logger
 # import logging
 # import time
-import pickle
-import pandas as pd
 # from datetime import datetime, timedelta
-from sklearn.cluster import KMeans
-from preprocessing import get_data, clean_data, add_features
-from config.import_config import import_config
-# from loguru import logger
 
 
-# @logger.catch
-# data preprocessing
-def train_kmeans(df: pd.DataFrame):
-    """Function trains the Kmeans model on the input data and returns it.
-    :param df: Train dataset
-    :return: Kmeans trained model
-    """
-    # config
-    cfg = import_config('config/params.yaml')
-    n_clusters = cfg['k_means']['n_clusters']
-    random_state = cfg['k_means']['random_state']
-    # data preprocessing
-    df_pickup = df[['pickup_longitude', 'pickup_latitude']]
-    df_dropoff = df[['dropoff_longitude', 'dropoff_latitude']]
-    cols = ['longitude', 'latitude']
-    df_pickup.columns = cols
-    df_dropoff.columns = cols
-    df_fit = pd.concat([df_pickup, df_dropoff], ignore_index=True)
-    kmeans = KMeans(n_clusters=n_clusters, random_state=random_state)
-    kmeans.fit(df_fit)
-    with open('models/kmeans.pkl', 'wb') as f:
-        pickle.dump(kmeans, f)
-    return kmeans
-
-
+# # @logger.catch
 # if __name__ == '__main__':
 #     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 #     path = '..\\train.csv'
@@ -56,3 +27,14 @@ def train_kmeans(df: pd.DataFrame):
 #     execution_time = str(timedelta(seconds=(end_time - start_time))).split('.')[0]
 #     logging.info(f'Execution time = {execution_time}')
 #     logging.info(f'K-means Trained & Saved. {datetime.utcfromtimestamp(end_time).strftime("%Y-%m-%d %H:%M:%S")}')
+
+from preprocessing import get_data, add_features, clean_data
+from config.import_config import import_config
+from models.train_kmeans import train_kmeans
+import pandas as pd
+
+df = get_data('train.csv')
+df = clean_data(df)
+
+model = train_kmeans(df)
+print(df.shape)
