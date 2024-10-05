2a7b1a15f6263cb467fe795346e4d47f02cbd2cf
hshsdrop <i1.kuznetsv@gmail.com>
2024-10-05 11:45:18 +0300

    add EDA.ipynb add kmeans model (models/kmeans.pkl and train_kmeans.py) add config/params.yaml (kmeans params) import_config.py add preprocessing.py (clean_data, get_data, add_features) add .gitignore add testing.ipynb

---
+++ models/train_kmeans.py
@@ -0,0 +1,59 @@
+# import logging
+# import time
+import pickle
+import pandas as pd
+# from datetime import datetime, timedelta
+from sklearn.cluster import KMeans
+from preprocessing import get_data, clean_data, add_features
+from config.import_config import import_config
+
+
+# data preprocessing
+def train_kmeans(df: pd.DataFrame):
+    """Function trains the Kmeans model on the input data and returns it.
+    :param df: Train dataset
+    :return: Kmeans trained model
+    """
+    # config
+    cfg = import_config('config/params.yaml')
+    n_clusters = cfg['k_means']['n_clusters']
+    random_state = cfg['k_means']['random_state']
+    # data preprocessing
+    df_pickup = df[['pickup_longitude', 'pickup_latitude']]
+    df_dropoff = df[['dropoff_longitude', 'dropoff_latitude']]
+    cols = ['longitude', 'latitude']
+    df_pickup.columns = cols
+    df_dropoff.columns = cols
+    df_fit = pd.concat([df_pickup, df_dropoff], ignore_index=True)
+    kmeans = KMeans(n_clusters=n_clusters, random_state=random_state)
+    kmeans.fit(df_fit)
+    with open('models/kmeans.pkl', 'wb') as f:
+        pickle.dump(kmeans, f)
+    return kmeans
+
+
+# if __name__ == '__main__':
+#     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+#     path = '..\\train.csv'
+#     logging.info('Training a model for taxi route clustering!')
+#     # Data: get, clean
+#     start_time = time.time()
+#     logging.info(f'Data preparation starts. {datetime.utcfromtimestamp(start_time).strftime("%Y-%m-%d %H:%M:%S")}')
+#     df = clean_data(get_data(path))
+#     end_time = time.time()
+#     execution_time = str(timedelta(seconds=(end_time - start_time))).split('.')[0]
+#     logging.info(f'Execution time = {execution_time}')
+#     logging.info(f'Dataset is ready for training. {datetime.utcfromtimestamp(end_time).strftime("%Y-%m-%d %H:%M:%S")}') 
+#     logging.info('Next Stage')
+#     # K-means fitting & saving
+#     start_time = time.time()
+#     logging.info(f'Fitting K-means. {datetime.utcfromtimestamp(start_time).strftime("%Y-%m-%d %H:%M:%S")}')
+#     model = train_kmeans(df)
+#     pickle.dump(model, open('models/kmeans.pkl', 'wb'))
+#     end_time = time.time()
+#     execution_time = str(timedelta(seconds=(end_time - start_time))).split('.')[0]
+#     logging.info(f'Execution time = {execution_time}')
+#     logging.info(f'K-means Trained & Saved. {datetime.utcfromtimestamp(end_time).strftime("%Y-%m-%d %H:%M:%S")}')
+
+# df = pd.read_csv('train.csv')
+# train_kmeans(df)
